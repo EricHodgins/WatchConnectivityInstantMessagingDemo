1faf8526e9177f8efe3f11ea685df19517133b59
Eric Hodgins <hodgins.e@gmail.com>
2016-10-29 20:02:06 -0400

    Initial Commit

---
+++ WatchConnectivityInstantMessagingDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchConnectivityInstantMessagingDemo WatchKit Extension
+//
+//  Created by Eric Hodgins on 2016-10-29.
+//  Copyright © 2016 Eric Hodgins. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
